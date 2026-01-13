7c4232c16535aa12cdade22b2ed8566b6fd743e9
abdessamadelouarrag <abdessamad4804@gmail.com>
2026-01-13 12:00:00 +0100

    test routing with simple method

--- routes/routes.php
+++ routes/routes.php
@@ -1,19 +1,19 @@
 <?php
 
-$action = $_GET['action'] ?? null;
+// $action = $_GET['action'] ?? null;
 
-if($action == 'coach'){
-    require __DIR__ . "/../view/Coach/dashboardCoach.php";
-}
-if($action == 'sportif'){
-    require __DIR__ . "/../view/Sportif/pageSportif.php";
-}
-if($action == 'login'){
-    require __DIR__ . "/../view/Auth/login.php";
-}
-if($action == 'add'){
-    require __DIR__ . "/../view/style/home.php";
-}
+// if($action == 'coach'){
+//     require __DIR__ . "/../view/Coach/dashboardCoach.php";
+// }
+// if($action == 'sportif'){
+//     require __DIR__ . "/../view/Sportif/pageSportif.php";
+// }
+// if($action == 'login'){
+//     require __DIR__ . "/../view/Auth/login.php";
+// }
+// if($action == 'add'){
+//     require __DIR__ . "/../view/style/home.php";
+// }
 
 $y = $_SERVER['REQUEST_URI'];
 
